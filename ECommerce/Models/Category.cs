014f1e9970b527fbd5ab36cf3bef0b36c3eda24f
felipe.santos <felipe.santos@bairesdev.com>
2023-07-19 18:36:28 -0300

    feat: adding database connectors, controler and view (for category)

---
+++ ECommerce/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } //keyName Id and CategoryId makes the EF understands it is the primary key
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
