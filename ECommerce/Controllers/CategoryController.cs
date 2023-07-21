0b4bfce520743aaf7cad81e07b0730f3e558c6b4
felipe.santos <felipe.santos@bairesdev.com>
2023-07-20 23:34:09 -0300

    feat: adding notifications

--- ECommerce/Controllers/CategoryController.cs
+++ ECommerce/Controllers/CategoryController.cs
@@ -34,6 +34,7 @@ namespace ECommerce.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -64,6 +65,7 @@ namespace ECommerce.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -96,6 +98,7 @@ namespace ECommerce.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index", "Category");
         }
     }
