77fc147cbc21bb3e90af563e6e99da3cfbcfae37
felipe.santos <felipe.santos@bairesdev.com>
2023-07-26 01:21:56 -0300

    feat: adding Product CRUD

---
+++ Ecommerce.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Models;
+
+
+namespace ECommerce.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
---
+++ Ecommerce.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.DataAccess.Data;
+using ECommerce.DataAccess.Repository.IRepository;
+using ECommerce.Models;
+
+
+namespace ECommerce.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
